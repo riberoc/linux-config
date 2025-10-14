4040cbba6c904020f3968042b813baea4dd29790
marcelo <marcelo.riberocerro@gmail.com>
2025-10-14 18:13:54 +0200

    init

---
+++ kitty-term.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install kitty
+sudo apt update
+sudo apt install kitty -y
+
+# Ensure the config directory exists
+mkdir -p ~/.config/kitty
+
+# Move your config
+mv -f ./configs/kitty.config ~/.config/kitty/kitty.conf
+
+# Set kitty as the default terminal non-interactively
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty 50
+sudo update-alternatives --set x-terminal-emulator /usr/bin/kitty
